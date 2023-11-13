b120fe27e24e7a882bc230835d62b83a6f4b3cf0
Falandy <Falandyjean@gmail.com>
2023-11-12 21:44:28 -0500

    adding audio on click a key

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-const touches = [...document.querySelectorAll(".bouton")]
-const listeKeycode = touches.map(touche => touche.dataset.key)
+document.addEventListener('DOMContentLoaded', function() {
+    const touches = [...document.querySelectorAll(".bouton")]
+    const listeKeycode = touches.map(touche => touche.dataset.key)
+
+
+
+
+    // Obtenez tous les boutons
+    document.querySelectorAll('.bouton').forEach(function (button) {
+        button.addEventListener('click', function () {
+            const audio = button.getAttribute('data-audio')
+            playSound(audio, 5.5)
+        })
+    })
+})
+
+
+function playSound(audio, playbackRate) {
+    son.playbackRate = playbackRate
+    son.play(audio)
+}
